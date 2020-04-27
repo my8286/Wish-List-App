91be5c117017c84bf6944d14092ffd8074c8574f
my8286 <manishyadav8286@gmail.com>
2020-04-28 00:53:49 +0530

    index.html test.js deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-module.exports = (firstName, lastName)=> {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.fullName = function () { 
-        return this.firstName + ' ' + this.lastName;
-    }
-}
